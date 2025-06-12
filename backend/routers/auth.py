5250a57af9d1199c5d1d9e697e65c7d4e769f2db
Sebastijan Dominis <sebastijan.dominis99@gmail.com>
2025-06-12 16:49:26 +0200

    Implemented basic authentication logic with argon2 and jwt.

---
+++ backend/routers/auth.py
@@ -0,0 +1,119 @@
+from fastapi import APIRouter, Depends, Path, HTTPException
+from database import SessionLocal
+from sqlalchemy.orm import Session
+from typing import Annotated
+from models import Users
+from passlib.context import CryptContext
+from starlette import status
+from pydantic import BaseModel, Field
+from jose import jwt, JWTError
+from datetime import datetime, timezone, timedelta
+from dotenv import load_dotenv
+import os
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"]
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+class CreateUserRequest(BaseModel):
+    email: str = Field(min_length=11, max_length=150)
+    password: str = Field(min_length=8, max_length=20)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "email": "john_doe@gmail.com",
+                "password": "johndoe123"
+            }
+        }
+    }
+
+argon2_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+def authenticate_user(email: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.email == email).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
+    if not argon2_context.verify(password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password.")
+    return user
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+def create_token(email: str, user_id: int):
+    encoding = {"sub": email, "id": user_id, "exp": datetime.now(timezone.utc) + timedelta(minutes=60)}
+    token = jwt.encode(encoding, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/authorize")
+
+def authorization(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        user_id: int = payload.get("id")
+
+        if email is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization Failed.")
+        
+        return {"email": email, "id": user_id}
+    
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization Failed.")
+
+@router.post("/create-user", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    try:
+        email = create_user_request.email
+        hashed_password = argon2_context.hash(create_user_request.password)
+
+        new_user = Users(email = email, hashed_password = hashed_password)
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return {"message": "User successfully created"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"User creation failed: {str(e)}")
+
+@router.post("/authorize", status_code=status.HTTP_204_NO_CONTENT)
+async def authorize_user(db: db_dependency, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    email = form_data.username
+    password = form_data.password
+
+    user = authenticate_user(email, password, db)
+
+    token = create_token(user.email, user.id)
+    return {"access_token": token, "token_type": "bearer"}
+
+
+# only for dev mode:
+#------------------------------------------------------------
+@router.get("/all-users")
+async def read_users(db: db_dependency):
+    return db.query(Users).all()
+
+@router.delete("/delete-user/{user_id}")
+async def delete_user(db: db_dependency, user_id: int = Path(ge=1)):
+    user = db.query(Users).filter(Users.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
+    db.delete(user)
+    db.commit()
+#------------------------------------------------------------
