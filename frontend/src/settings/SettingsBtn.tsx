49df6280e7c22b6c8ee9a4ed9b0b17e4c6b95532
Sebastijan Dominis <sebastijan.dominis99@gmail.com>
2025-06-17 19:25:45 +0200

    Added the same outline on focus styling for the buttons missed earlier

--- frontend/src/settings/SettingsBtn.tsx
+++ frontend/src/settings/SettingsBtn.tsx
@@ -8,7 +8,7 @@ interface SettingsBtnProps {
 function SettingsBtn({ onClick, children }: SettingsBtnProps) {
   return (
     <button
-      className="w-14 rounded-lg bg-[#d2d2d2] px-2 py-1 text-sm md:w-20 md:px-4 md:py-2 md:text-base lg:w-28 lg:text-lg"
+      className="w-14 rounded-lg bg-[#d2d2d2] px-2 py-1 text-sm outline-none focus:ring focus:ring-[#d2d2d2] focus:ring-offset-2 md:w-20 md:px-4 md:py-2 md:text-base lg:w-28 lg:text-lg"
       onClick={onClick}
     >
       {children}
